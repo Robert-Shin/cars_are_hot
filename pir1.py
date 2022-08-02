5ecd0d57d3e501a735402b13d58d200307fec6ec
Robert-Shin <35202617+Robert-Shin@users.noreply.github.com>
2022-08-02 00:04:22 -0400

    Add files via upload

---
+++ pir1.py
@@ -0,0 +1,10 @@
+from gpiozero import MotionSensor
+from time import sleep
+pir = MotionSensor(4)
+sleep(50)
+print("detecting...")
+pir.wait_for_motion()
+while True:
+	pir.wait_for_motion()
+	print("hi mom")
+	sleep(5)
